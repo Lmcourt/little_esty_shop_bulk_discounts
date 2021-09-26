c5cfee6704a401d015a46dae56f0db6622d8ecbe
Laura Court <laura.mcourt@gmail.com>
2021-09-26 15:43:17 -0400

    Validates fields and tests relationships

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:merchant) }
+    it { should have_many(:items).through(:merchant) }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:bulk_name)}
+    it {should validate_presence_of(:percentage_discount)}
+    it {should validate_presence_of(:quantity_threshold)}
+  end
 end
