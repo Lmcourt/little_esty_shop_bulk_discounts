a9cf20d0ca5d0ea882e6982efef4b503359466d6
Laura Court <laura.mcourt@gmail.com>
2021-09-27 00:15:14 -0400

    Adds discounted revenue for merchant

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -8,9 +8,12 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :status }
   end
+
   describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_one(:merchant).through(:item)}
+    it { should have_many(:bulk_discounts).through(:merchant)}
   end
 
   describe "class methods" do
@@ -37,6 +40,33 @@ RSpec.describe InvoiceItem, type: :model do
     end
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+  end
+
+  describe 'discounted items' do
+    before :each do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2)
+      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 5, unit_price: 10, status: 1)
+
+      @bd1 = @merchant1.bulk_discounts.create!(bulk_name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
+      @bd2 = @merchant1.bulk_discounts.create!(bulk_name: "Discount B", percentage_discount: 10, quantity_threshold: 5)
+    end
+
+    it 'has a total revenue' do
+      expect(@ii_1.revenue).to eq(100)
+    end
+
+    it 'finds highest discount for an item' do
+      expect(@ii_1.highest_discount).to eq(@bd1)
+    end
+
+    it 'has a discounted price' do
+      expect(@ii_1.discounted_price).to eq(80)
     end
   end
 end
