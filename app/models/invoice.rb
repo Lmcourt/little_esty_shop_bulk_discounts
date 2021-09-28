b93de07408dd58aff170f99c303ff595c2ba74ed
Laura Court <laura.mcourt@gmail.com>
2021-09-28 15:10:09 -0400

    Adds merchant total revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,6 +14,12 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
+  def merchant_total_revenue(merchant)
+    invoice_items.joins(:item)
+                 .where('items.merchant_id = ?', merchant)
+                 .sum('invoice_items.unit_price * quantity')
+  end
+
   def discounted_revenue
     invoice_items.sum do |ii|
       if ii.highest_discount
@@ -22,5 +28,21 @@ class Invoice < ApplicationRecord
         ii.revenue
       end
     end
+  end
+
+  # def discounted_revenue(merchant_id)
+  #   invoice_items.sum do |ii|
+  #     if ii.highest_discount && ii.merchant.id == merchant_id
+  #       ii.discounted_price
+  #     else
+  #       ii.revenue
+  #     end
+  #   end
+  # end
+
+  def merchant_discounted_revenue(merchant)
+    invoice_items.joins(:items)
+                .where('items.merchant_id = ?', merchant)
+                .sum(merchant_total_revenue(merchant) - invoice_items.discounted_price)
   end
 end
