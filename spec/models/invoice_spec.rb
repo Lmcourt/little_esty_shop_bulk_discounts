7161d3cd43392b0975aa78fda1c91a4c67444750
Laura Court <laura.mcourt@gmail.com>
2021-09-28 15:10:18 -0400

    Adds merchant total revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -16,11 +16,14 @@ RSpec.describe Invoice, type: :model do
   describe "instance methods" do
     before :each do
       @merchant1 = Merchant.create!(name: 'Hair Care')
+      @merchant2 = Merchant.create!(name: 'Place')
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_2 = Item.create!(name: "stuff", description: "things", unit_price: 10, merchant_id: @merchant2.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
       @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
       @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 2, unit_price: 10, status: 2)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 2, unit_price: 10, status: 2)
       @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 10, unit_price: 10, status: 1)
 
       @bd1 = @merchant1.bulk_discounts.create!(bulk_name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
@@ -28,7 +31,11 @@ RSpec.describe Invoice, type: :model do
     end
 
     it "total_revenue" do
-      expect(@invoice_1.total_revenue).to eq(120)
+      expect(@invoice_1.total_revenue).to eq(140)
+    end
+
+    it 'has merchant total revenue' do
+      expect(@invoice_1.merchant_total_revenue(@merchant2)).to eq(20)
     end
   end
 
